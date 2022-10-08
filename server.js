9437db462c3c14f8978923879835ee241a54c4f4
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-10-08 17:16:56 +0800

    users can see messages according user connections

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
-  console.log('New WS Connection...');
+  
+  // (Line below) once connected, this line sends out an event (in this case, the event name is 'message' but you can choose any name you want) and message which will be received from the client JS
+  // (Line below) only the user sees this message
+  // Welcome current user
+  socket.emit('message', 'Welcome to ChatCord!');
+
+  // Broadcast when a user connects
+  // (Line below) anyone but the user sees the message
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    // (Line below) all users can see the message
+    io.emit('message', 'A user has left the chat');
+  });
 });
 
 const PORT = 3000 || process.env.PORT;

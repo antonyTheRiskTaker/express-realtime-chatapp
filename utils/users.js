aad8e94a7c6af15580b2015e9bf9f70fab0071fb
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-10-09 09:57:08 +0800

    corrected bug and return the deleted user

--- utils/users.js
+++ utils/users.js
@@ -19,7 +19,8 @@ function userLeave(id) {
   const index = users.findIndex(user => user.id === id);
 
   if (index !== -1) {
-    return users.splice(index, 1);
+    // (Line below) it returns an array of deleted elements
+    return users.splice(index, 1)[0];
   }
 }
 

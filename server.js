56e329c08f0670c96c6067938d541602fbc16884
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-10-09 09:10:06 +0800

    rearranged socketio event handlers into joinRoom event

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http'); // This package is needed to use socket.io
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,25 +16,26 @@ const botName = 'ChatCord Bot';
 
 // Run when client connects
 io.on('connection', socket => {
-  
-  // (Line below) once connected, this line sends out an event (in this case, the event name is 'message' but you can choose any name you want) and message which will be received from the client JS
-  // (Line below) only the user sees this message
-  // Welcome current user
-  socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+  socket.on('joinRoom', ({ username, room }) => {
+    // (Line below) once connected, this line sends out an event (in this case, the event name is 'message' but you can choose any name you want) and message which will be received from the client JS
+    // (Line below) only the user sees this message
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
-  // Broadcast when a user connects
-  // (Line below) anyone but the user sees the message
-  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
-
-  // Runs when client disconnects
-  socket.on('disconnect', () => {
-    // (Line below) all users can see the message
-    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    // Broadcast when a user connects
+    // (Line below) anyone but the user sees the message
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
   });
 
   // Listen for chatMessage (see client JS)
   socket.on('chatMessage', msg => {
     io.emit('message', formatMessage('USER', msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    // (Line below) all users can see the message
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
 });
 
